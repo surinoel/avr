646275c3ca09d804440c489351d54f92c4ea838f
surinoel <yyj48921@gmail.com>
2019-04-14 16:56:34 +0900

    first commit

---
+++ intro/intro/intro/main.c
@@ -0,0 +1,18 @@
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+	DDRA = 0xff;
+	int i;
+	int n = 8;
+    while (1) 
+    {
+		for(i=0; i<(1<<n); i++) {
+			PORTA = i;
+			_delay_ms(300);				
+		}
+    }
+}
+
