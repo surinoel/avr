43256914fa96e63d2bf17aad282165e034d46979
surinoel <48958763+surinoel@users.noreply.github.com>
2019-06-22 01:36:24 +0900

    Update main.c

--- i2c/ATS75D8/ATS75D8/main.c
+++ i2c/ATS75D8/ATS75D8/main.c
@@ -14,15 +14,15 @@ int main(void)
 {
 	fnd_init();
 	uart0_init();
-    i2c_init();
+	i2c_init();
 	uint8_t ht, lt;
 	uint16_t temp;
-    while (1) 
-    {
+	while (1) 
+	{
 		while(i2c_readReg(0x98, 0x00, &ht, &lt) == 0) {
 			temp = (ht<<8) | lt;
 			printf("temp:%d\r\n",temp);
 		}
-    }
+	}
 }
 
